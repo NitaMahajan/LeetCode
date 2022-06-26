3fbbe6a517ff7d4aec8370a96d16e7889c7a5f0d
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-06-26 12:37:45 +0530

    Time: 5 ms (80.47%), Space: 42.3 MB (85.08%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,28 +1,21 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int memoize[] = new int[s.length()+1];
-        return wordBreak(s, wordDict,memoize, 0);
-    }
-    public boolean wordBreak(String s, List<String> wordDict, int[] memoize,int index) {
-        if(index == s.length()) {
-            return true;
-        }
-        for(String word: wordDict) {
-            int i = index, j = 0;
-            while((i < s.length() && j < word.length()) && (s.charAt(i) == word.charAt(j))) {
-                i++; j++;
-            }
-            if(j == word.length()) {// we can use the word
-                int res = memoize[i];
-                if(res == 0) {
-                    memoize[i] = wordBreak(s, wordDict, memoize,i) ? 1 : -1;
+        int n = s.length();
+        boolean memoize[] = new boolean[n + 1];
+        memoize[n] = true;
+        for(int i = n-1; i >= 0; i--) {
+            for(String word: wordDict) {
+                if(word.length() > (n-i)) continue;
+                int itr = i, j = 0;
+                while((itr < n && j < word.length()) && (s.charAt(itr) == word.charAt(j))) {
+                    itr++; j++;
                 }
-                if(memoize[i] == 1) {
-                    return true;
+                if(j == word.length()) {
+                    memoize[i] = memoize[i + word.length()];
+                    if(memoize[i]) break;
                 }
             }
         }
-        memoize[index] = -1;
-        return false;
+        return memoize[0];
     }
 }
