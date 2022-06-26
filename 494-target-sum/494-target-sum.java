a0156b4016bd53806409b28d0c7f3fec2b7885ad
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-06-26 11:21:17 +0530

    Time: 1083 ms (5.00%), Space: 52.1 MB (21.46%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,15 +1,46 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int ans = calculate(nums, 0, 0, target);
+        int sum = getSum(nums);
+        int memoize[][] = new int[nums.length + 1][2*sum + 1];
+        initialize(memoize);
+        int ans = calculate(nums, memoize, sum, 0, 0, target);
         return ans;
     }
     
-    public int calculate(int nums[], int index, int sum, int target) {
+    public int calculate(int nums[], int[][] memoize, int len, int index, int sum, int target) {
         
         if(index == nums.length){
-            return (sum == target) ? 1 : 0;
+            if(sum == target) {
+                memoize[index][normalize(sum, len)] = 1;
+                return 1;
+            } else {
+                memoize[index][normalize(sum, len)] = 0;
+                return 0;
+            }
         }
-        
-        return calculate(nums,index+1,sum+nums[index],target) + calculate(nums,index+1,sum-nums[index],target);
+        int left = memoize[index + 1][normalize(sum+nums[index], len)];
+        int right = memoize[index + 1][normalize(sum-nums[index], len)];
+        if(left == -1) left = calculate(nums,memoize,len,index+1,sum+nums[index],target);
+        if(right == -1) right = calculate(nums,memoize,len,index+1,sum-nums[index],target);
+        return left + right;
+    }
+    
+    public int getSum(int nums[]) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+    public int normalize(int sum, int size) {
+        return sum + size;
+    }
+    
+    public void initialize(int[][] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = -1;
+            }
+        }
     }
 }
