c3ac268289e4aa79b77a38c7963f1c42d7f7daae
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-06-28 10:31:26 +0530

    Time: 147 ms (5.04%), Space: 53.1 MB (7.23%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        Arrays.sort(nums);
+        for(int i: nums) {
+            if(!count.containsKey(i)) {
+                count.put(i, 1);
+            }
+            else {
+                count.put(i, count.get(i) + 1);
+            }
+        }
+        int n = nums.length;   
+        int dp[]=new int[n+1];
+        dp[n] = 0;
+        
+        int i=n-1;
+        while(i >= 0) {
+            if((i < n-1) && nums[i] == nums[i + 1]) {
+                dp[i] = dp[i + 1];
+                i--; continue;
+            }
+            int profit = 0;
+            if((i == nums.length-1) || (nums[i] + 1 != nums[i+1])) {
+                profit = (count.get(nums[i]) * nums[i]) + dp[i+1];
+            } else {
+                profit = Math.max((count.get(nums[i]) * nums[i]) + dp[i+count.get(nums[i+1])+1],dp[i+1]);
+            }
+            dp[i] = profit;
+            i--;
+        }
+        for(int j=0;j<=n;j++) System.out.print(dp[j] + " ");
+        System.out.println();
+        return dp[0];
+        
+    }
+}
