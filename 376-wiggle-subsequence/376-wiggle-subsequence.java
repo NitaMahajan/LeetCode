fffb25316521d0c81e16b41b96d0f24cc912dcc8
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-07-10 23:51:58 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (6.74%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,27 +1,16 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
         int n = nums.length;
-        if(n==1) return 1;
-        if(n==2 && (nums[0] != nums[1])) return 2;
-        if(n==2 && (nums[0] == nums[1])) return 1;
-        int diff[] = new int[n-1];
-        boolean isZero = true;
-        for(int i=0;i < n-1;i++) {
-            diff[i] = nums[i+1]-nums[i];
-            if(diff[i] != 0) isZero=false;
-        }
-        if(isZero) return 1;
-        int len = 1; 
-        int lastDiff=diff[0];
-        for(int i=1;i<n-1;i++) {
-            if(lastDiff == 0) {
-                lastDiff = diff[i];
-            }
-            else if((lastDiff>0 && diff[i] < 0) || (lastDiff<0 && diff[i] > 0)) {
-                len++;
-                lastDiff = diff[i];
+        if(n < 2) return n;
+        int previous = nums[1] - nums[0]; 
+        int length = previous == 0 ? 1: 2;
+        for(int i=2;i<n;i++) {
+            int current = nums[i] - nums[i-1];
+            if((current < 0 && previous >= 0) || (current > 0 && previous <= 0)) {
+                length++;
+                previous = current;
             }
         }
-        return len+1;
+        return length;
     }
 }
