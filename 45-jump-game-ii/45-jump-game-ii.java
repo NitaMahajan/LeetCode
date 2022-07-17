426af91d9202886e5aff2ad31012428a52ce310d
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-07-17 22:49:09 +0530

    Time: 1 ms (99.93%), Space: 42.8 MB (89.45%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,20 +1,16 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return 0;
-        int dp[]= new int[n];
-        dp[n-1]=0;
-        for(int i = n-2;i>=0;i--) {
-            int minDis = 100001;
-            if(nums[i] == 0) {
-                dp[i] = minDis;
-                continue;
+        int jumps = 0, currEnd = 0, currFarthest = 0;
+        for(int i = 0; i < nums.length - 1; i++) {
+            currFarthest = Math.max(currFarthest, nums[i] + i);
+            if(i == currEnd) {
+                jumps++;
+                currEnd = currFarthest;
             }
-            for(int j=i+1; j <= Math.min(i + nums[i], n-1); j++) {
-                minDis = Math.min(minDis, 1 + dp[j]);
-            }
-            dp[i] = minDis;
         }
-        return dp[0];
+        return jumps;
     }
 }
+
+//[2,3,1,1,4]
+//curEnd = 4, currFarthest=4, jumps=2
