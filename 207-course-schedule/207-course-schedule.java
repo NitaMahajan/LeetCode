b9e238214fdb01474175ee239336a3891676e9ca
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-08-12 14:41:41 +0530

    Time: 6 ms (75.74%), Space: 47.7 MB (65.27%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,38 +1,42 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>(); // Adjacency list
-        int degree[] = new int[numCourses];
-        for(int i=0; i < numCourses; i++) { // Initialize an array list for each node
-            graph.add(new ArrayList<>());
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<numCourses;i++) {
+            list.add(new ArrayList<>());
         }
-        for(int i=0;i < prerequisites.length; i++) {
+        for(int i=0;i<prerequisites.length;i++) {
             int from = prerequisites[i][1], to = prerequisites[i][0];
-            graph.get(from).add(to); // Prepare a graph
-            degree[to]++; // Increase indegree for to node
-        }
-        Queue<Integer> queue = new LinkedList<Integer>(); // Queue for BFS
-        int count = 0; // Count the number of nodes traversed in the topological sort
-        // Perform bfs on nodes which have zero indegree
-        for(int i = 0; i < numCourses; i++) {
-            if(degree[i] == 0) {
-                queue.add(i);
-                count++;
-            }
+            list.get(from).add(to);
         }
         
-        while(!queue.isEmpty()) {
-            Integer node = queue.poll();
-            for(Integer adj: graph.get(node)) {
-                degree[adj]--;
-                if(degree[adj] == 0) {
-                    queue.add(adj);
-                    count++;
+        int visited[] = new int[numCourses];
+        int dfsVisited[] = new int[numCourses];
+        
+        for(int i=0;i<numCourses;i++) {
+            if(visited[i] == 0) {
+                
+                if(dfsCycle(i, visited, dfsVisited, list) == true) {
+                    return false;
                 }
             }
         }
-        if(count == numCourses) return true;
+        return true;
+    }
+    public boolean dfsCycle(int node, int visited[], int dfsVisited[], List<List<Integer>> list) {
+        dfsVisited[node] = 1;
+        visited[node] = 1;
+        List<Integer> neighbors = list.get(node);
+        for(Integer adj: neighbors) {
+            if(visited[adj] == 0) {
+                if(dfsCycle(adj, visited, dfsVisited, list) == true) {
+                    return true;
+                }
+            } else if(dfsVisited[adj] == 1) {
+                return true;
+            }
+        }
+        dfsVisited[node] = 0;
         return false;
-        
     }
     
 }
