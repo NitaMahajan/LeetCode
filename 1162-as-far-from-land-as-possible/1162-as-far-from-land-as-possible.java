c13a760f04f6ff504e05bc4a92562dd45665916d
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-08-21 23:08:23 +0530

    Time: 26 ms (62.34%), Space: 66.7 MB (19.30%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,28 +1,34 @@
 class Solution {
     class Node{
-        private int x; private int y; private int d;
+        private int x; 
+        private int y; 
+        private int d;
         Node(int x, int y, int d) {
-            this.x = x; this.y = y; this.d = d;
+            this.x = x; 
+            this.y = y; 
+            this.d = d;
         }
         int getX(){ return this.x; }
         int getY(){ return this.y; }
         int getD(){ return this.d; }
     }
+    
     int directions[] = {0,1,0,-1,0};
     public int maxDistance(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         Queue<Node> queue = new LinkedList<>();
-        // Add all land cells to the bfs queue
-        for(int i=0;i<m;i++) {
+        for(int i=0;i<m;i++) { // Add all land cells to the bfs queue
             for(int j=0;j<n;j++) {
                 if(grid[i][j] == 1) {
                     queue.add(new Node(i,j,0));
                 }
             }
         }
-        // If there are no water cells
-        if(queue.size() == m*n) return -1;
-        int depth = -1;
+        
+        if(queue.size() == m*n) return -1; // If there are no water cells return -1
+        
+        // Keep track of depth of each water cell so that we can have depth of the farthest water cell by the end of BFS
+        int depth = -1; 
         while(!queue.isEmpty()) {
             Node curr = queue.poll();
             depth = Math.max(depth, curr.getD());
