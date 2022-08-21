86a21c031720acddad1b5e578df7a8794bfa8d6f
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-08-21 23:06:58 +0530

    Time: 29 ms (51.57%), Space: 66.8 MB (18.73%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -0,0 +1,39 @@
+class Solution {
+    class Node{
+        private int x; private int y; private int d;
+        Node(int x, int y, int d) {
+            this.x = x; this.y = y; this.d = d;
+        }
+        int getX(){ return this.x; }
+        int getY(){ return this.y; }
+        int getD(){ return this.d; }
+    }
+    int directions[] = {0,1,0,-1,0};
+    public int maxDistance(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        Queue<Node> queue = new LinkedList<>();
+        // Add all land cells to the bfs queue
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j] == 1) {
+                    queue.add(new Node(i,j,0));
+                }
+            }
+        }
+        // If there are no water cells
+        if(queue.size() == m*n) return -1;
+        int depth = -1;
+        while(!queue.isEmpty()) {
+            Node curr = queue.poll();
+            depth = Math.max(depth, curr.getD());
+            for(int i=0;i<4;i++) {
+                int nr = directions[i] + curr.getX();
+                int nc = directions[i + 1] + curr.getY();
+                if(nr < 0 || nr >= m || nc < 0 || nc >= n || grid[nr][nc] == 1) continue;
+                grid[nr][nc] = 1;
+                queue.add(new Node(nr, nc, curr.getD() + 1));
+            }
+        }
+        return depth;
+    }
+}
