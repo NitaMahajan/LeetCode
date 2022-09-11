06694119c0b53ce9d10d672ae69279e06e2c5e9d
Nita Mahajan <36067443+NitaMahajan@users.noreply.github.com>
2022-09-11 11:51:11 +0530

    Time: 33 ms (16.27%), Space: 53.8 MB (70.05%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,25 +1,46 @@
 class Solution {
-    int directions[] = {0,1,0,-1,0};
+    int DIR[] = {0,1,0,-1,0};
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        int cache[][]=new int[m][n];
-        int max=-1;
+        int indegree[][] = new int[m][n];
+        Queue<Node> queue = new LinkedList<>();
+        // Prepare Indegree Matrix and Push 0 Indegree Nodes to queue
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                max = Math.max(max, dfs(i, j, m, n, matrix, cache));
+                for(int d=0;d<4;d++) {
+                    int r = DIR[d]+i, c = DIR[d+1]+j;
+                    if(r < 0 || r >= m || c < 0 || c >= n) continue;
+                    if(matrix[r][c] < matrix[i][j]) 
+                        indegree[i][j]++;
+                }
+                if(indegree[i][j] == 0) {
+                    queue.offer(new Node(i,j,1));
+                }
             }
         }
-        return max;
-    }
-    public int dfs(int r, int c, int m, int n, int [][]matrix, int [][]cache) {
-        if(cache[r][c] != 0) return cache[r][c];
-        int max=1;
-        for(int i=0;i<4;i++) {
-            int nr = directions[i]+r, nc = directions[i+1]+c;
-            if(nr < 0 || nr >= m || nc < 0 || nc >= n || matrix[nr][nc] >= matrix[r][c]) continue;
-            max = Math.max(max, dfs(nr, nc, m, n, matrix, cache)+1);
+        int maxDistance = 0;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int x = node.getX(), y = node.getY(), dis = node.getDis();
+            maxDistance = Math.max(maxDistance, dis);
+            for(int d=0;d<4;d++) {
+                int r = DIR[d]+x, c = DIR[d+1]+y;
+                if(r < 0 || r >= m || c < 0 || c >= n || matrix[r][c] <= matrix[x][y]) continue;
+                indegree[r][c]--;
+                if(indegree[r][c] == 0) {
+                    queue.offer(new Node(r,c,dis+1));
+                }
+            }
         }
-        cache[r][c] = max;
-        return max;
+        return maxDistance;
     }
+}
+class Node {
+    private int x,y,dis;
+    Node(int x, int y, int dis) {
+        this.x = x; this.y = y; this.dis = dis;
+    }
+    public int getX(){ return this.x; }
+    public int getY(){ return this.y; }
+    public int getDis(){ return this.dis; }
 }
